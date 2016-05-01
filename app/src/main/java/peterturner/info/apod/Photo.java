0d636b5953e335bb587d0683552b81c4c9000e0e
pmturner2 <pmturner2@gmail.com>
2016-05-01 13:01:20 -0700

    Some updates to Photo and PhotoListActivity after moving to Windows

--- app/src/main/java/peterturner/info/apod/Photo.java
+++ app/src/main/java/peterturner/info/apod/Photo.java
@@ -7,19 +7,14 @@ import java.util.Date;
  */
 public class Photo {
     public String url;
-    public String hdUrl;
     public String title;
-    public String mediaType;
-    public String serviceVersion;
     public String explanation;
     public Date date;
 
-    public Photo(String url, String hdUrl, String title, String mediaType, String serviceVersion, String explanation) {
-        this.url = url;
-        this.hdUrl = hdUrl;
-        this.title = title;
-        this.mediaType = mediaType;
-        this.serviceVersion = serviceVersion;
-        this.explanation = explanation;
+    // Optional - some are public domain
+    public String copyright;
+
+    public Photo() {
+
     }
 }
--- app/src/main/java/peterturner/info/apod/PhotoListActivity.java
+++ app/src/main/java/peterturner/info/apod/PhotoListActivity.java
@@ -27,29 +27,29 @@ public class PhotoListActivity extends AppCompatActivity {
     // Nasa API Key
     private static final String API_KEY = "8xLqaN9LQZVF6a7kAi1kupue3xIXVz1M45ga5bxe";
 
+    // How many photos to fetch beyond what is needed for current views.
+    private static final int FETCH_THRESHOLD = 4;
+
     // Base Url
     String mBaseUrl = "https://api.nasa.gov/planetary/apod?api_key=" + API_KEY;
 
-    TextView mTitle;
     PhotoAdapter mAdapterPhotos;
     ArrayList<Photo> mPhotos;
     int mHighestFetched = -1;
+    SimpleDateFormat mDateFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_list);
 
-        mTitle = (TextView) findViewById(R.id.tvTitle);
         mPhotos = new ArrayList<Photo>();
         mAdapterPhotos = new PhotoAdapter(this, mPhotos);
+        mDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
         // Populate initial 5 cells.
         Log.d("PETER", "initial population");
-        for (int i = 0; i < 5; ++i) {
-            fetchPhoto(i);
-            mHighestFetched = 4;
-        }
+        fetchPhotoRange(0, FETCH_THRESHOLD);
 
         ListView lv = (ListView) findViewById(R.id.lvPhotos);
         lv.setAdapter(mAdapterPhotos);
@@ -63,15 +63,23 @@ public class PhotoListActivity extends AppCompatActivity {
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
                 Log.d("PETER", "onScroll first " + firstVisibleItem + " mHighest: " + mHighestFetched);
                 int lastVisibleItem = firstVisibleItem + visibleItemCount;
-                if (lastVisibleItem > mHighestFetched) {
-                    for (int i = mHighestFetched + 1; i <= lastVisibleItem; ++i) {
-                        fetchPhoto(i);
-                    }
-                    mHighestFetched = lastVisibleItem;
+                if (lastVisibleItem >= mHighestFetched - FETCH_THRESHOLD) {
+                    fetchPhotoRange(mHighestFetched + 1, mHighestFetched + FETCH_THRESHOLD);
                 }
                 Log.d("PETER", "onScroll last: " + lastVisibleItem);
             }
         });
+    }
+
+    private void fetchPhotoRange(final int startIndex, final int endIndex) {
+        if (mHighestFetched >= startIndex) {
+            Log.e("Error", "Re-fetching data: Highest fetched " + mHighestFetched + " startIndex " + startIndex + " endIndex " + endIndex);
+        }
+        mPhotos.ensureCapacity(endIndex);
+        for (int i = startIndex; i <= endIndex; ++i) {
+            fetchPhoto(i);
+        }
+        mHighestFetched = endIndex;
     }
 
     private void fetchPhoto(final int index) {
@@ -81,8 +89,7 @@ public class PhotoListActivity extends AppCompatActivity {
         Calendar calendar = new GregorianCalendar(TimeZone.getDefault(), Locale.getDefault());
         calendar.add(Calendar.DAY_OF_YEAR, offsetInDays);
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String formatted = dateFormat.format(calendar.getTime());
+        String formatted = mDateFormat.format(calendar.getTime());
 
         String url = mBaseUrl + "&date=" + formatted;
         AsyncHttpClient client = new AsyncHttpClient();
@@ -95,24 +102,22 @@ public class PhotoListActivity extends AppCompatActivity {
                 Log.i("PETER", "Success " + index);
 
                 try {
-                    Photo photo = new Photo(
-                            response.getString("url"),
-                            response.getString("hdurl"),
-                            response.getString("title"),
-                            response.getString("media_type"),
-                            response.getString("service_version"),
-                            response.getString("explanation")
-                    );
+                    Photo photo = new Photo();
+                    photo.url = response.getString("url");
+                    photo.title = response.getString("title");
+                    photo.explanation = response.getString("explanation");
 
                     String dateString = response.getString("date");
                     SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
                     try {
                          photo.date = format.parse(dateString);
-
                     } catch (ParseException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
+
+                    photo.copyright = response.optString("copyright");
+
                     if (index < mPhotos.size()) {
                         mPhotos.add(index, photo);
                     } else {
